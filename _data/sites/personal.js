0fa292bc16b451b49ba1eca44ce1bb31a01071f5
Phillip Gourley <phillipgourley@Phillips-iMac.local>
2021-05-27 21:54:17 +0100

    Adding new site lists and deleting initial config.

--- _data/sites/personal.js
+++ _data/sites/personal.js
@@ -1,8 +1,8 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	description: "Personal",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
@@ -12,15 +12,6 @@ module.exports = {
 		freshChrome: "site",
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://mugandsnug.com/"
 	]
 };
